53b0d4e8847147042b4b1ec14d53374318dc8112
diego <dsruggeri@gmail.com>
2022-05-18 11:35:07 -0300

    proyecto backend creado

---
+++ backend/src/test/java/com/portfolio/dsr/DsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
